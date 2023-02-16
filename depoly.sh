8524e7d5fe63fad38c9a0bc8cb391cec57877190
season <997432833lixuan@gmail.com>
2023-02-16 10:11:58 +0800

    chore: delay

--- depoly.sh
+++ depoly.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd /home/ubuntu/app/blog && git pull 
+cd /home/ubuntu/app/blog && git pull
 pnpm install && pnpm docs:build
 rm -r ./public
-mv ./docs/.vitepress/dist ./public 
+mv ./docs/.vitepress/dist ./public
